4614da1c60dab9cb8e27e0d16b345ce1a6747b85
William Maddocks <willislearning23@gmail.com>
2024-10-09 23:09:52 +0100

    Scanning: Whitespace

--- app/main.py
+++ app/main.py
@@ -75,8 +75,6 @@ def main():
                 print("SLASH / null")
         elif c == " " or c == "\r" or c == "\t":
             pass
-        elif c == "\n":
-            ptr += 1
         else:
             error = True
             line_number = file_contents.count('\n', 0, file_contents.find(c)) + 1
