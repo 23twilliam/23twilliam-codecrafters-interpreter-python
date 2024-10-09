d9cf70ded36017155f8fd06454d6d343848a382d
William Maddocks <willislearning23@gmail.com>
2024-10-09 23:28:04 +0100

    Scanning: Multi-line errors

--- app/main.py
+++ app/main.py
@@ -75,7 +75,7 @@ def main():
                 print("SLASH / null")
         elif c == " " or c == "\r" or c == "\t" or c == "\n":
             ptr += 1
-            continue
+            pass
         else:
             error = True
             line_number = file_contents.count('\n', 0, file_contents.find(c)) + 1
