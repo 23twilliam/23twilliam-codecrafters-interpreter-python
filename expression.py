45f2d8d1274513492400fc788012045be604d56e
William Maddocks <willislearning23@gmail.com>
2025-01-28 01:51:30 +0000

    hello

--- expression.py
+++ expression.py
@@ -5,9 +5,8 @@ def Unary(operator, right):
     return f"({operator.lexeme} {right})"
 
 def Literal(expr):
-    if expr == None:
-        return "nil"
-    return str(expr).lower()
+    def __init__(self, value):
+        self.value = value
 
 def Grouping(expr):
     return f"(group {expr})"
