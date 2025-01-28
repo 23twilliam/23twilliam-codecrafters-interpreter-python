a403bceaaa9565372bb05d2e70d2fd0c3b780be3
William Maddocks <willislearning23@gmail.com>
2025-01-28 01:49:58 +0000

    test

--- parser_.py
+++ parser_.py
@@ -61,7 +61,7 @@ class Parser:
         if self.match(["NUMBER", "STRING"]):
             return Literal(self.previous().literal)
         if self.match(["LEFT_PAREN"]):
-            expr = expression()
+            expr = self.expression()
             self.consume("RIGHT_PAREN", "Unmatched parentheses.")
             return Grouping(expr)
         self.error(self.peek(), "Expect expression")
